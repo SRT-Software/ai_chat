4eba24a5d2d47f0557d290d9eeb57f8daadbfd13
lh <1411186296@qq.com>
2023-11-19 01:03:23 +0800

    lh

--- backend/chat.py
+++ backend/chat.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, origins='*', resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 text_list = []
 source_list = []
@@ -26,33 +26,36 @@ QUES_TEMPLATE = 'make 1 relative question about {}' \
 def check_token():
     # 获取请求头中的 token
     print(request.headers)
-    token = request.headers.get('Authorization')
-    print(token)
+    token = request.headers.get('Access-Control-Request-Headers')
+    headers = token.split(',')
     # 校验 token
-    if token != 'Bearer test':
+    if "authorization" not in headers :
         abort(401)  # 返回 401 Unauthorized 错误
 
 
-@app.route('/api/data', methods=['POST'])
+@app.route('/api/data', methods=['POST', 'OPTIONS'])
 def chatbot():
-    data = request.json
-    ques = data.get('question')
-    globals()["text_list"], globals()["source_list"] = match_query(ques, database="milvus")
-    sse_data = zhipuai.model_api.sse_invoke(
-        model="chatglm_pro",
-        prompt=[
-            {"role": "user", "content": QA_TEMPLATE.format(text_list, ques)},
-        ],
-        temperature=0.95,
-        top_p=0.7,
-        incremental=True
-    )
+    if request.method  == 'OPTIONS':
+        print(request.headers)
+    else:
+        data = request.json
+        ques = data.get('question')
+        globals()["text_list"], globals()["source_list"] = match_query(ques, database="milvus")
+        sse_data = zhipuai.model_api.sse_invoke(
+            model="chatglm_pro",
+            prompt=[
+                {"role": "user", "content": QA_TEMPLATE.format(text_list, ques)},
+            ],
+            temperature=0.95,
+            top_p=0.7,
+            incremental=True
+        )
 
-    def generate():
-        for event in sse_data.events():
-            yield f"data: {event.data}\n\n"
+        def generate():
+            for event in sse_data.events():
+                yield f"data: {event.data}\n\n"
 
-    return Response(generate(), mimetype='text/event-stream')
+        return Response(generate(), mimetype='text/event-stream')
 
 
 @app.route('/api/source', methods=['GET'])

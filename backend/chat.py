4f189e9ae64cd74c763603a8c20ca7b5fb8f3bd4
lh <1411186296@qq.com>
2023-11-19 00:50:15 +0800

    lh

--- backend/chat.py
+++ backend/chat.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, origins='*')
+CORS(app, origins='*', resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 text_list = []
 source_list = []

cb8d9ab9456228469e4b36dc145746c56eb5a86b
lh <1411186296@qq.com>
2023-11-26 10:22:07 +0800

    lh

--- backend/view/main.py
+++ backend/view/main.py
@@ -1,17 +1,3 @@
 from flask import Blueprint
 from flask import Flask, request, jsonify, Response, abort, Blueprint
 main = Blueprint('api', __name__)
-
-@main.before_request
-def check_token():
-    # 获取请求头中的 token
-    token = request.headers.get('Access-Control-Request-Headers')
-    if token is not None:
-        headers = token.split(',')
-        if "authorization" not in headers:
-            abort(401)  # 返回 401 Unauthorized 错误
-    else:
-        token = request.headers.get('Authorization')
-        if token != 'Bearer test':
-            abort(401)  # 返回 401 Unauthorized 错误
-    # 校验 token

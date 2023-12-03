e741220ab70b89187df90b9d19e31ce26dc584a1
chl <3102727009@qq.com>
2023-12-03 21:10:24 +0800

    chl

--- chat/app/page.tsx
+++ chat/app/page.tsx
@@ -4,6 +4,7 @@ import {ChatProvider} from "@/app/context/chatContext";
 import React, { useState } from 'react'
 import FileDialog from "./components/FileDialog";
 
+
 export default function Home() {
 
     return (

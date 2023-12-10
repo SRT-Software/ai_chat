72df17b66ea93637715c784055da6b846b59e289
lh <1411186296@qq.com>
2023-12-10 17:36:14 +0800

    lh

--- chat/next.config.js
+++ chat/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    serverRuntimeConfig: {
+        // 在这里配置你的服务器选项
+        // 指定公网 IP 和端口号
+        // 例如，使用公网 IP 0.0.0.0 和端口号 3000
+        // 修改 HOST 和 PORT 变量以适应你的需求
+        HOST: '0.0.0.0',
+        PORT: 3000,
+    },
+};
